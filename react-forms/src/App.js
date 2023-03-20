d4501987f29986254ed84ca5d88f7ed6fcefad3e
Olga <olga.sleepless@gmail.com>
2023-03-20 16:08:55 -0400

    Update Form

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import Form from "./form";
 
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div>
+      <h1>Hello from App</h1>
+      <Link to="/form">Form</Link>
+
+      <Switch>
+        <Route path='/form' component={Form}/>
+      </Switch>
+    </div>
+
   );
 }
 
--- react-forms/src/form.jsx
+++ react-forms/src/form.jsx
@@ -1,8 +1,7 @@
 import { useState, useRef } from "react";
+import './form.css'
 
 const Form = () => {
-  const passwordFilled = useRef(null)
-
   const [user, setUser] = useState( {
     firstName: "",
     lastName: "",
@@ -12,30 +11,79 @@ const Form = () => {
     staff: "", 
     bio: "", 
     signUpNotification: ""
-  })
-
+  });
+  
+  const [errors, setErrors] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const currentErrors = validate();
+    if (currentErrors.length) {
+      console.log('ERRORS!');
+    } else {
+      console.log('No Errors');
+    } 
+  };
+  
+  const handleChange = (field) => {
+    return (e) => {
+      const value = e.target.value
+      setUser({
+        ...user,
+        [field]: value
+      });
+    } 
+  };
+  const validate = () => {
+    const currentErrors = [];
+    if (user.firstName.length === 0) currentErrors.push('First Name is missing')
+    if (user.lastName.length === 0) currentErrors.push('Last Name is missing')
+    if (user.phoneNumber.length > 0 && user.phoneType.value === '') currentErrors.push('Phone type must be selected')
+    if (user.bio.length > 280) currentErrors.push('Bio should have a character limit of 280 characters')
+    return currentErrors;
+    debugger
+  };
+  
   return(
     <>
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="First Name" value={user.firstName} onChange={handleChange('firstName')}/>
-      <input type="text" placeholder="Last Name" value={user.lastName} onChange={handleChange('lastName')}/>
-      <input type="email" placeholder="Email" value={user.email} onChange={handleChange('email')}/>
-      <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number" value={user.phoneNumber} onChange={handleChange('phoneNumber')}/>
-      <label for="Phone Type">Choose Phone Type</label>
-      <select id="phoneType" size="3" multiple vaule={user.phoneType} onChange={handleChange("phoneType")}>
-        <option value="home">Home</option>
-        <option value="work">Work</option>
-        <option value="mobile">Mobile</option>
-      </select>
-      <input type="radio" id="instructor" value={user.staff} onChange={handleChange("staff")}/>
-        <label for="instructor">Instructor</label>
+      <ul>
+        {errors.map((error,i) => <li key={i}>{error}</li>)}
+    console.log(user.phoneType);
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="First Name" value={user.firstName} onChange={handleChange('firstName')}/>
+        <br></br>
+        <input type="text" placeholder="Last Name" value={user.lastName} onChange={handleChange('lastName')}/>
+        <br></br>
+        <input type="email" placeholder="Email" value={user.email} onChange={handleChange('email')}/>
+        <br></br>
+        <input type="tel" placeholder="123-123-1234" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={user.phoneNumber} onChange={handleChange('phoneNumber')}/>
+        <br></br>
+        
+        {/* Drop down */}
+        <label for="Phone Type">Select Phone Type  </label>
+        <select id="phoneType"  size="1" vaule={user.phoneType} onChange={handleChange("phoneType")}>
+          <option value="" disabled selected>Select your option</option>
+          <option value="home">Home</option>
+          <option value="work">Work</option>
+          <option value="mobile">Mobile</option>
+        </select>
+        
+        <br></br>
+        <input type="radio" id="instructor" name="staff" value={user.staff} onChange={handleChange("staff")}/>
+          <label for="instructor">Instructor</label>
 
-        <input type="radio" id="student" value={user.staff} onChange={handleChange("staff")}/>
-        <label for="student">Student</label>
-        <input type="textarea" placeholder="Bio" value={user.bio} onChange={handleChange("bio")}/>
+        <input type="radio" id="student" name="staff" value={user.staff} onChange={handleChange("staff")}/>
+          <label for="student">Student</label>
+        
+        <br></br>
+        <textarea placeholder="Bio" value={user.bio} onChange={handleChange("bio")}></textarea>
+        <br></br>
         <input type="checkbox" id="signUpNotification" name="sign up notification" value={user.signUpNotification} onChange={handleChange("signUpNotification")}/>
         <label for="signUpNotification">Sign up for email notifications</label>
-    </form>
+
+        <br></br>
+        <input type="submit" value="submit" />
+      </form>
     </>
   )
 }
